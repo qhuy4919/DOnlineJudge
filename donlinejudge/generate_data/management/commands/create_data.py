311e1be6d9cdc1a998b29b2340f764e482014b7d
Qhuy <huy.tran@gotecq.com>
2023-11-28 17:27:49 +0700

    feat: problem generated

--- donlinejudge/generate_data/management/commands/create_data.py
+++ donlinejudge/generate_data/management/commands/create_data.py
@@ -1,11 +1,12 @@
 from django.core.management.base import BaseCommand
-from utils.generate_data import generate_user, generate_tag
+from utils.generate_data import generate_user, generate_tag, generate_problem
 
 
 class Command(BaseCommand):
     help = "Create sample data"
 
     def handle(self, *arg, **kwargs):
-        generate_user(100)
+        # generate_user(100)
         # generate_tag()
+        generate_problem(10)
         print("complete!!!")
--- donlinejudge/utils/generate_data.py
+++ donlinejudge/utils/generate_data.py
@@ -1,10 +1,15 @@
 from faker import Faker
 from accounts.models import User
-from problem.models import ProblemTag
+from problem.models import ProblemTag, Problem
 import random
 import json
 
 fake = Faker()
+
+
+def pick_random_admin_user():
+    admin_id = User.objects.get(is_superuser=True)
+    return admin_id
 
 
 def generate_user(num: int):
@@ -13,6 +18,31 @@ def generate_user(num: int):
             username=fake.user_name(), email=fake.email(), password="12345678"
         )
         user.save()
+
+
+def generate_problem(num: int):
+    DEFAULT_STATEMENT = "Takahashi wants a beverage called AtCoder Drink in a restaurant. It can be ordered at a regular price of P yen.\r\nHe also has a discount coupon that allows him to order it at a lower price of \r\nQ yen. However, he must additionally order one of the restaurant's \r\nN dishes to use that coupon. For each \r\ni=1,2,…,N, the price of the \r\ni-th dish is \r\nD i yen.\r\n\r\nPrint the minimum total amount of money that he must pay to get the drink.\r\nInput\r\nN P Q \r\nD 1 ​ D 2 ​ … D N ​\r\nOutput\r\nPrint the answer."
+    DEFAULT_TAG = [{"id": 1, "tag_name": "uncategorized"}]
+    DEFAULT_SAMPLE_TEST = [
+        {"input": "3 100 50\n60 20 40\n", "output": "70"},
+        {"input": "3 100 50\n60000 20000 40000\n", "output": "100"},
+    ]
+    DEFAULT_TEST_ZIP = "/media/tests/1.zip"
+
+    for i in range(num):
+        problem = Problem.objects.create(
+            display_id=fake.uuid4(),
+            created=fake.date_time(),
+            is_visible=True,
+            author=pick_random_admin_user(),
+            title="Order Something Else " + str(i),
+            statement=DEFAULT_STATEMENT,
+            difficulty="Easy",
+            source=None,
+            sample_test=DEFAULT_SAMPLE_TEST,
+            test_zip=DEFAULT_TEST_ZIP,
+        )
+        problem.save()
 
 
 def generate_tag():
